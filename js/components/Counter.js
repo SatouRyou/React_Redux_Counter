0312c450ebfcf7214fb45c16e4516a7ccdf4c37f
SatouRyou <satouryou1205@gmail.com>
2016-10-24 00:23:29 +0900

    カウンターの完成

--- js/components/Counter.js
+++ js/components/Counter.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react';
 
 export default class Counter extends Component {
   render() {
+    const { value, onIncrement, onDecrement } = this.props;
     return (
       <div>
-        <h3>Counter: </h3>
-        <button>
+        <h3>Counter: {value}</h3>
+        <button onClick={onIncrement}>
           +
         </button>
         {' '}
-        <button>
+        <button onClick={onDecrement}>
           -
         </button>
       </div>
