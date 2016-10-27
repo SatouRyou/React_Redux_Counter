190543a5f9572aa519ab3ebc385ca58a12189000
SatouRyou <satouryou1205@gmail.com>
2016-10-27 14:03:44 +0900

    Counter完成

--- js/index.js
+++ js/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Counter from './components/Counter';
+import counter from './reducers/index';
+import { createStore } from 'redux';
+
+const store = createStore(counter);
 
 function render() {
   ReactDOM.render(
-    <Counter />,
+    <Counter
+    value={store.getState()}
+      onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
+      onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
+      />,
     document.getElementById('main')
   );
 }
 
 render();
+store.subscribe(render);
