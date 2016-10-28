c3bb68d9d30b715b046e70145e294b06bc6f254c
SatouRyou <satouryou1205@gmail.com>
2016-10-28 23:41:31 +0900

    詳細説明用のコメント追加

--- js/index.js
+++ js/index.js
@@ -11,12 +11,17 @@ function render() {
   ReactDOM.render(
     <Counter
     value={store.getState()}
+      // 加算処理のイベント
       onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
+      // 引算処理のイベント
       onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
       />,
     document.getElementById('main')
   );
 }
 
+// 初回描画
 render();
+
+// storeに描画関数を保存
 store.subscribe(render);
